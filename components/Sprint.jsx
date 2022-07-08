dcb2bc63ea9310d36eafb9db1c8031d4df0cd307
RiceDefender <nghiadao2002@gmail.com>
2022-07-08 10:55:53 +0200

    Fix node import. Add stratisics

--- components/Sprint.jsx
+++ components/Sprint.jsx
@@ -1,76 +1,81 @@
 import { StatusBar } from 'expo-status-bar';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
 import { NavigationContainer, StackRouter, createNati } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Pedometer } from 'expo-sensors';
 import HomeScreen from './Home.jsx';
 
+import { VictoryBar, VictoryChart, VictoryAxis, VictoryTheme, VictoryArea } from 'victory-native';
 
-class Steps extends React.Component {
 /*
-  state = {
-    isPedometerAvailable: 'checking',
-    pstep: 0,
-    cstep: 0,
-  };
-  componentDidMount() {
-    this.subscribe();
-  }
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  subscribe = () => {
-    const end = new Date();
-    const start = new Date();
-    this.subscription = Pedometer.watchStepCount(result => {
-      this.setState({
-        cstep: result.steps,
-      });
-    });
-
-    start.setDate(end.getDate() - 1);
-    subscription = Pedometer.watchStepCount(result => {
-      this.setState({
-        cstep: result.steps,
-      });
-    });
-
-
-
-    Pedometer.getStepCountAsync(start, end).then(
-      result => {
-        this.setState({ pstep: result.steps });
-      },
-      error => {
-        this.setState({
-          pstep: 'Could not get stepCount: ' + error,
-        });
-      }
-    );
-
-    unsubscribe = () => {
-      this.subscription && this.subscription.remove();
-      this.subscription = null;
+class Steps extends React.Component {
+  
+    state = {
+      isPedometerAvailable: 'checking',
+      pstep: 0,
+      cstep: 0,
     };
-    
-  }
-  render(){
-    return (
-      <View style={{ alignItems: "center", justifyContent: "center" }}>
-        <Text>Text Screen</Text>
-        <Text>You walked: {this.state.pstep} steps in the last 24 hours</Text>
-        <Text>CONGRATS YOU'VE WALKED GORGEOUS: {this.state.cstep} STEPS</Text>
-      </View>
-    );
-  }
-*/
+    componentDidMount() {
+      this.subscribe();
+    }
+    componentWillUnmount() {
+      this.unsubscribe();
+    }
+  
+    subscribe = () => {
+      const end = new Date();
+      const start = new Date();
+      this.subscription = Pedometer.watchStepCount(result => {
+        this.setState({
+          cstep: result.steps,
+        });
+      });
+  
+      start.setDate(end.getDate() - 1);
+      subscription = Pedometer.watchStepCount(result => {
+        this.setState({
+          cstep: result.steps,
+        });
+      });
+  
+  
+  
+      Pedometer.getStepCountAsync(start, end).then(
+        result => {
+          this.setState({ pstep: result.steps });
+        },
+        error => {
+          this.setState({
+            pstep: 'Could not get stepCount: ' + error,
+          });
+        }
+      );
+  
+      unsubscribe = () => {
+        this.subscription && this.subscription.remove();
+        this.subscription = null;
+      };
+      
+    }
+    render(){
+      return (
+        <View style={{ alignItems: "center", justifyContent: "center" }}>
+          <Text>Text Screen</Text>
+          <Text>You walked: {this.state.pstep} steps in the last 24 hours</Text>
+          <Text>CONGRATS YOU'VE WALKED GORGEOUS: {this.state.cstep} STEPS</Text>
+        </View>
+      );
+    }const [dataarray, setdataarray] = useState({});
+  */
+/*
   state = {
     isPedometerAvailable: 'checking',
     pastStepCount: 0,
     currentStepCount: 0,
+    datarray: [],
   };
+  //datarray = this.state{[]};
 
   componentDidMount() {
     this._subscribe();
@@ -80,10 +85,13 @@ class Steps extends React.Component {
     this._unsubscribe();
   }
 
+  
+
   _subscribe = () => {
     this._subscription = Pedometer.watchStepCount(result => {
       this.setState({
         currentStepCount: result.steps,
+        datarray: [...datarray, result.steps]
       });
     });
 
@@ -107,25 +115,96 @@ class Steps extends React.Component {
     this._subscription && this._subscription.remove();
     this._subscription = null;
   };
+  */
 
+/*
   render() {
+    
     return (
+
       <View style={{ alignItems: "center", justifyContent: "center" }}>
         <Text>You walked: {this.state.pastStepCount} steps in the last 24 hours</Text>
         <Text>CONGRATS YOU'VE WALKED GORGEOUS: {this.state.currentStepCount} STEPS</Text>
+
+            <VictoryChart
+            theme={VictoryTheme.material}
+          >
+            <VictoryArea
+              style={{ data: { fill: "#c43a31" } }}
+              data={this.state.datarray}
+            />
+          </VictoryChart>
+
       </View>
     );
   }
 }
+*/
 
 
 
 
 
 
+function SprintScreen() {
+  const [pastStepCount, setPastStepCount] = useState(0);
+  const [currentStepCount, setCurrentStepCount] = useState(0);
+  const [dataarray, setDataarray]= useState([]);
 
-function SprintScreen({ navigation }) {
+  useEffect(()=>{
+    setDataarray(array => [...array, currentStepCount])
+    console.log("TEST")
+  }, [currentStepCount] )
+
+  const _subscribe = () => {
+    subscription = Pedometer.watchStepCount(result => {
+      setCurrentStepCount(result.steps);
+      
+    });
+    
+
+    const end = new Date();
+    const start = new Date();
+    start.setDate(end.getDate() - 1);
+    Pedometer.getStepCountAsync(start, end).then(
+      result => {
+        setPastStepCount(result.steps);
+      },
+      error => {
+        setPastStepCount('Could not get stepCount: ' + error);
+      }
+    );
+  };
+
+  const _unsubscribe = () => {
+    // this.subscription && this.subscription.remove();
+    // this.subscription = null;
+  };
+
+
+  useEffect(()=>{
+    _subscribe();
+    return ()=> _unsubscribe();
+  },[])
+
+  return (
+
+    <View style={{ alignItems: "center", justifyContent: "center" }}>
+      <Text>You walked: {pastStepCount} steps in the last 24 hours</Text>
+      <Text>CONGRATS YOU'VE WALKED GORGEOUS: {currentStepCount} STEPS</Text>
+
+          <VictoryChart
+          theme={VictoryTheme.material}
+        >
+          <VictoryArea
+            style={{ data: { fill: "#c43a31" } }}
+            data={dataarray}
+          />
+        </VictoryChart>
+
+    </View>
+  );
 
 
 }
-export default Steps;
+export default SprintScreen;
